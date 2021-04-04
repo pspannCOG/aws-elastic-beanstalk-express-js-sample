199e5a7512496706e19c93cfec6f3a5e797a9845
pspannCOG <patrick.spann@cognizant.com>
2021-04-04 09:50:40 -0700

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('2021 is the year patrick w. spann takes charge of his life!'));
+app.get('/', (req, res) => res.send('Bean and Rascal will co-exist peacefully!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
